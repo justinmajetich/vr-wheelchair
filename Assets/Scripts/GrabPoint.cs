dd52e43159d15679ae68ad3e1eaa6f33c75d4575
justinmajetich <justinmajetich@gmail.com>
2021-04-23 18:06:40 -0400

    Re-write GrabPoint class to self-configure its defaults

---
+++ Assets/Scripts/GrabPoint.cs
@@ -0,0 +1,27 @@
+using UnityEngine.XR.Interaction.Toolkit;
+
+/// <summary>
+/// Provides a disposable grab point to mediate interaction with a VRWheelControl.
+/// The GrabPoint will destroy it's GameObject when no longer selected.
+/// GrabPoint inherits from XRGrabInteractable, part of Unity's XR Interaction Toolkit.
+/// </summary>
+public class GrabPoint : XRGrabInteractable
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // Configure interactable defaults.
+        movementType = MovementType.VelocityTracking;
+        trackRotation = false;
+        throwOnDetach = false;
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+
+        // Destroy grab point object on selection end.
+        Destroy(gameObject);
+    }
+}
